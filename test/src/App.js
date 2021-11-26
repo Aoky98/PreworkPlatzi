3d63ef3a594139cb7f52f746ebb5099315d6d7d3
AokyCarmona <aokyadlai@gmail.com>
2021-11-25 21:24:25 -0600

    cambios

--- test/src/App.js
+++ test/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edita esto <code>src/App.js</code> and save to reload.
+          probando cosas shidoris <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
